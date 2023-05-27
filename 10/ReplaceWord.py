974dceffb0551d90a3da1d894b626b3a5201046e
Rebecca Senn <rsenn1@users.noreply.github.com>
2023-05-26 19:10:25 -0500

    Create ReplaceWord.py

---
+++ 10/ReplaceWord.py
@@ -0,0 +1,12 @@
+# Complete the function to replace the word WGU with Western Governors University
+# and print the new string
+
+def replaceWGU(mystring):
+    replaced = mystring.replace('WGU', 'Western Governors University')
+    print(replaced)
+    
+# expected output: Western Governors University Rocks
+replaceWGU('WGU Rocks')
+    
+# expected output: Hello, Western Governors University
+replaceWGU('Hello, WGU')
